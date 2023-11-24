f0add3b681eec0071d542c9908234fa9f6867726
Bakhtiyar Kainolla <kenjopa6@gmail.com>
2023-11-24 10:53:29 +0500

    add alt to image to fix building process

--- app/page.tsx
+++ app/page.tsx
@@ -18,9 +18,9 @@ export default function Page() {
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+              Next.js Test Template
             </a>
-            , brought to you by Vercel.
+            , using Vercel.
           </div>
           <Link
             href="/login"
